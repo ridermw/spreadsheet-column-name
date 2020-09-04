ced500e97912517b423d6ac9bcebd96ccf886fb8
Matthew Williams <mattheww@gmail.com>
2020-09-04 06:44:39 -0400

    more cleanup

--- console/Program.cs
+++ console/Program.cs
@@ -16,26 +16,15 @@ namespace spreadsheet_column_name
             // edge error check.  should be a non-negative int
             if (columnNumber < 0) return "";
             var returnValue = "";
-            
-            //alphabet is like a BASE 26 number...so lets try converting.
-
             var remainder = 0;
-            
+            //alphabet is like a BASE 26 number...so lets try converting.                
             while (columnNumber >= 0)
             {
-                var addedChar = '0';
                 remainder = columnNumber % 26;
-                Console.WriteLine("Begin: " + columnNumber + " : " + remainder + " : " + returnValue);
-                
-                addedChar = (char)(remainder + 'A');
-            
-                returnValue = addedChar + returnValue; // concat to the left.  Big AH HA! moment for me.
+                returnValue = (char)(remainder + 'A') + returnValue; // concat to the left.  Big AH HA! moment for me.
                 columnNumber /= 26;
                 columnNumber--;  // This took me forever to figure out! I was always off by one until I did this.
-                Console.WriteLine("End: " + columnNumber + " : " + returnValue);
             }
-
-            Console.WriteLine(returnValue);
 
             return returnValue;
         }
